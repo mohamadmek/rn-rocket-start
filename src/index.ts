78a78f912075ad96331b4a180b05fc4bb3c5ccce
Mohamad Meksasi <45299765+mohamadmek@users.noreply.github.com>
2024-12-09 19:54:24 +0100

    Add command basic example

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
-const hello = () => {
-  console.log("Hello World! www");
-};
+import { Command } from "commander";
 
-console.log("Hello World! www");
+const program = new Command();
+
+program.name("mycli").description("My CLI").version("0.0.1");
+
+program.argument("<string>", "Name of the user").action((message: string) => {
+  console.log(`Hello, ${message}!`);
+});
+
+program.parse(process.argv);
